d9364b21e1a4290cbb67edf6047e2dcd1e7c1fc5
hp <Shivraj.Jadhav@external.hella.com>
2019-05-31 00:21:46 +0530

    Commit v1.2

--- app/src/main/java/com/example/tripsbarchart/DataClass.java
+++ app/src/main/java/com/example/tripsbarchart/DataClass.java
@@ -2,17 +2,20 @@ package com.example.tripsbarchart;
 
 public class DataClass {
 
-    String date;
-    String event_name;
-    String value;
+    private String date;
+    private String event_name;
+    private String value;
+    private String trip_no;
 
-    public DataClass(String date,String event_name,String value){
+    public DataClass(String date,String event_name,String value, String trip_no){
         this.date = date;
         this.event_name = event_name;
         this.value = value;
+        this.trip_no = trip_no;
     }
 
     public String getDate(){return date;}
     public String getEvent_name(){return event_name;}
     public String getValue(){return value;}
+    public String getTrip_no(){return trip_no;}
 }
--- app/src/main/java/com/example/tripsbarchart/MainActivity.java
+++ app/src/main/java/com/example/tripsbarchart/MainActivity.java
@@ -9,9 +9,17 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -22,29 +30,72 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.TimeZone;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnChartValueSelectedListener {
 
-    public static ArrayList<DataClass> data_array =  new ArrayList<>();
+    private ArrayList<DataClass> data_array =  new ArrayList<>();
+
+    private HashMap<Integer,Float> score_trips_array = new HashMap<>();
+
+    private HashMap<Integer,Float> hb = new HashMap<>();
+
+    private HashMap<Integer,Float> ha = new HashMap<>();
+
+    private HashMap<Integer,Float> dos = new HashMap<>();
+
+    private HashMap<Integer,Float> os = new HashMap<>();
+
+    private String current_date;
+
+    final String x_Dates[] = new String[10];
+
+    //ArrayList<String> x_label = new ArrayList<>();
+
+    private String TAG ="Main Activity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String current_date = getCurrentTime();
+        current_date = getCurrentTime();
 
         String value = substract_required_days(10);
         Log.i("Main Activity", current_date);
-        Log.i("Main Activity", current_date.substring(0,6));
-        Log.i("Main Activity", value);
+        //Log.i("Main Activity", current_date.substring(0,6));
+        //Log.i("Main Activity", value);
+
         new GetData().execute();
 
 
 
 
     }
+
+
+
+    public String getCurrentTime() {
+        String dateFormat = "yyyy-MM-dd";
+        String ourdate;
+
+        try {
+
+            Date value = new Date();
+            TimeZone timeZone = TimeZone.getTimeZone("Asia/Kolkata");
+            SimpleDateFormat dateFormatter = new SimpleDateFormat(dateFormat, Locale.UK); //this format changeable
+            dateFormatter.setTimeZone(timeZone);
+            ourdate = dateFormatter.format(value);
+            //ourdate = ourdate.substring(11,19);
+            //Log.d("OurDate", OurDate);
+        } catch (Exception e) {
+            ourdate = "00/00/0000 00:00:00";
+        }
+        return ourdate;
+    }
+
 
     public String substract_required_days(int n){
 
@@ -70,28 +121,27 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public String getCurrentTime() {
-        String dateFormat = "ddMMyy HH:mm:ss";
-        String ourdate;
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
 
-        try {
+        BarEntry be =(BarEntry)e;
 
-            Date value = new Date();
-            TimeZone timeZone = TimeZone.getTimeZone("Asia/Kolkata");
-            SimpleDateFormat dateFormatter = new SimpleDateFormat(dateFormat, Locale.UK); //this format changeable
-            dateFormatter.setTimeZone(timeZone);
-            ourdate = dateFormatter.format(value);
-            //ourdate = ourdate.substring(11,19);
-            //Log.d("OurDate", OurDate);
-        } catch (Exception e) {
-            ourdate = "00/00/0000 00:00:00";
-        }
-        return ourdate;
+        Log.i("Inside Click Value","Label:" + e.getX());
+
+        extract_day_values(x_Dates[Math.round(e.getX())]);
+
     }
+
+    @Override
+    public void onNothingSelected() {
+
+    }
+
 
     private class GetData extends AsyncTask<Void, Void, String> {
 
-        private String request_url = "https://version2-0.azurewebsites.net/api/Data-Last7Days?code=SRBSZmAzPMRkrSWIuvPYmu/0pB8b8MwOMFSNTYajfyQQ/F00LNdboQ==&name=2019-05-29T18:30:00.000Z";
+        private String request_url_base = "https://version2-0.azurewebsites.net/api/Data-Last7Days?code=SRBSZmAzPMRkrSWIuvPYmu/0pB8b8MwOMFSNTYajfyQQ/F00LNdboQ==&name=";
+        private String request_url = request_url_base + current_date + "T18:30:00.000Z";
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
@@ -116,7 +166,7 @@ public class MainActivity extends AppCompatActivity {
             // Making a request to url and getting response
             String json_str = sh.makeServiceCall(request_url);
 
-            Log.e("Main Activity", "Response from url: " + json_str);
+            //Log.e("Main Activity", "Response from url: " + json_str);
 
 
             return json_str;
@@ -146,14 +196,18 @@ public class MainActivity extends AppCompatActivity {
 
                     for(int i=0;i<jsonArray.length();i++){
 
-                        String value1 = jsonArray.getJSONObject(i).getJSONObject("Value1").getString("_");
+                        String value_1 = jsonArray.getJSONObject(i).getJSONObject("Value1").getString("_");
 
-                        String value2 = jsonArray.getJSONObject(i).getJSONObject("Value3").getString("_");
+                        String value_2 = jsonArray.getJSONObject(i).getJSONObject("Value3").getString("_");
 
-                        String value3 = jsonArray.getJSONObject(i).getJSONObject("Value4").getString("_");
+                        String value_3 = jsonArray.getJSONObject(i).getJSONObject("Value4").getString("_");
 
-                        data_array.add(new DataClass(value1, value2, value3));
-                        Log.i("Main Activity","Value1:" + value1 + "Value3:" + value2 + "Value3:" + value3);
+                        String value_4 = jsonArray.getJSONObject(i).getJSONObject("Value5").getString("_");
+
+                        //Log.i("Main Activity","Object:" + jsonObject);
+                        data_array.add(new DataClass(value_1,value_2,value_3,value_4));
+
+                        Log.i("Main Activity","Value1:" + value_1 + "Value2:" + value_2 + "Value3:" + value_3 + "Value4:" + value_4);
                     }
 
                     plotbarcharts();
@@ -190,7 +244,10 @@ public class MainActivity extends AppCompatActivity {
     private void plotbarcharts(){
 
         BarChart barChart = findViewById(R.id.barChart);
+
         ArrayList<BarEntry> dataVals = new ArrayList<>();
+
+
 
         Log.i("Main Activity","Size:" + data_array.size());
 
@@ -198,41 +255,198 @@ public class MainActivity extends AppCompatActivity {
 
             String past_Date = substract_required_days(i);
 
+            Log.i(TAG,"Past Date:" + past_Date);
 
+
+            float value =0;
+            int index_no = -(i-7);
+            x_Dates[index_no] = past_Date;
             for(int j=0;j<data_array.size();j++){
+
 
                 if(data_array.get(j).getDate().equals(past_Date)){
 
-                    int index_no = -(i-7);
 
-                    if(data_array.get(j).getEvent_name().equals("Score_Trip")) {
 
-                        Log.i("Inside Score_Trip","Value:" + data_array.get(j).getValue());
-                        dataVals.add(new BarEntry(index_no, Float.parseFloat(data_array.get(j).getValue())));
+                    if(data_array.get(j).getEvent_name().equals("Total_Score")) {
+
+
+
+                        value = Float.parseFloat(data_array.get(j).getValue());
+                        Log.i("Inside Score_Trip","Index:" + index_no + "Value:" + data_array.get(j).getValue());
+                        dataVals.add(new BarEntry(index_no,value));
                     }
 
+
                 }
+
+            }
+
+            if(value == 0){
+                value = 0;
+                Log.i("Outside Score_Trip","Index:" + index_no + "Value:" + value);
+                dataVals.add(new BarEntry(index_no,value));
             }
         }
 
-        BarDataSet dataSet = new BarDataSet(dataVals, "This is a bar chart");
-        dataSet.setColor(Color.rgb(0,0,255));
+        BarDataSet dataSet = new BarDataSet(dataVals, "Daily Score");
+        dataSet.setColor(Color.rgb(15,35,100));
         dataSet.setDrawValues(false);
 
         BarData barData = new BarData(dataSet);
 
+        barData.setBarWidth(0.6f);
+
         barChart.setData(barData);
 
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setTextSize(11f);
+        xAxis.setXOffset(0);
+        xAxis.setYOffset(0);
+
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
+
+            //private String[] qualities = new String[]{"1","2","3","4","5","6","7"};
+            @Override
+
+            public String getFormattedValue(float value, AxisBase axis) {
+                return x_Dates[(int)value % x_Dates.length];
+            }
+        });
+
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setGranularityEnabled(true);
+        xAxis.setAxisLineColor(Color.BLACK);
+        xAxis.setTextColor(Color.BLACK);
+        xAxis.setAxisLineWidth(3f);
+
+        YAxis yl = barChart.getAxisLeft();
+        yl.setAxisMinimum(-4);
+        barChart.getLegend().setEnabled(false);
+        barChart.setOnChartValueSelectedListener(this);
         barchart_properties(barChart);
+    }
+
+
+    private void  extract_day_values(String value){
+
+
+        Log.i("Inside extract day","value:" + data_array.get(0).getValue());
+        Log.i("Date_Value", value);
+        ha.clear();hb.clear();dos.clear();os.clear();score_trips_array.clear();
+        for(int j=0;j<data_array.size();j++){
+
+
+            if(data_array.get(j).getDate().equals(value)){
+
+                Log.i("Inside extract day","value:" + data_array.get(j).getValue());
+                Log.i("Value of j","J:" + j);
+                    switch(data_array.get(j).getEvent_name()){
+
+                        case "Score_Trip":
+
+                            score_trips_array.put(Integer.parseInt(data_array.get(j).getTrip_no()),Float.parseFloat(data_array.get(j).getValue()));
+                            break;
+
+                        case "Harsh_Braking":
+                            Log.i("HB","Value:" +  Integer.parseInt(data_array.get(j).getValue()));
+                            hb.put(Integer.parseInt(data_array.get(j).getTrip_no()), Float.parseFloat(data_array.get(j).getValue()));
+                            break;
+
+                        case "Harsh_Acc":
+                            Log.i("HA","Value:" +  Float.parseFloat(data_array.get(j).getValue()));
+                            ha.put(Integer.parseInt(data_array.get(j).getTrip_no()), Float.parseFloat(data_array.get(j).getValue()));
+                            break;
+
+                        case "Dangerous_Overspeeding":
+                            Log.i("DOS","Value:" +  Float.parseFloat(data_array.get(j).getValue()));
+                            dos.put(Integer.parseInt(data_array.get(j).getTrip_no()), Float.parseFloat(data_array.get(j).getValue()));
+                            break;
+
+                        case "Overspeeding":
+                            Log.i("OS","Value:" +  Float.parseFloat(data_array.get(j).getValue()));
+                            os.put(Integer.parseInt(data_array.get(j).getTrip_no()), Float.parseFloat(data_array.get(j).getValue()));
+                            break;
+
+                        default:
+                            break;
+
+                    }
+
+                }
+
+        }
+
+        Log.i(TAG,"Size Score_trips" + score_trips_array.size());
+
+        BarChart barChart_trips = findViewById(R.id.barChart_1);
+
+        ArrayList<BarEntry> data_vals_trips = new ArrayList<>();
+
+        final String[] trip_labels = new String[score_trips_array.size()];
+        for(int i=0;i<score_trips_array.size();i++){
+
+            Log.i("Plot Trips","Value" + hb.get(i) + " "  + ha.get(i) +  " " + dos.get(i) + " " +os.get(i));
+            data_vals_trips.add(new BarEntry(i,new float[]{hb.get(i),ha.get(i),dos.get(i),os.get(i)}));
+
+            trip_labels[i] = "Trip" + i;
+
+        }
+
+        int[] color_array = new int[]{Color.BLACK,Color.RED,Color.GREEN,Color.DKGRAY};
+
+        BarDataSet dataSet = new BarDataSet(data_vals_trips, "Trip Data");
+        dataSet.setColors(color_array);
+
+
+        dataSet.setDrawValues(false);
+
+        BarData barData = new BarData(dataSet);
+
+        barData.setBarWidth(0.6f);
+
+        barChart_trips.setData(barData);
+
+        XAxis xAxis = barChart_trips.getXAxis();
+        xAxis.setTextSize(11f);
+        xAxis.setXOffset(0);
+        xAxis.setYOffset(0);
+
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
+
+            //private String[] qualities = new String[]{"1","2","3","4","5","6","7"};
+            @Override
+
+            public String getFormattedValue(float value, AxisBase axis) {
+                return trip_labels[(int)value % trip_labels.length];
+            }
+        });
+
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setGranularityEnabled(true);
+        xAxis.setAxisLineColor(Color.BLACK);
+        xAxis.setTextColor(Color.BLACK);
+        xAxis.setAxisLineWidth(3f);
+
+        YAxis yl = barChart_trips.getAxisLeft();
+        //yl.setAxisMinValue(0);
+        yl.setAxisMinimum(0);
+
+        Legend l = barChart_trips.getLegend();
+        String[] label_array = new String[]{"Harsh Braking", "Harsh Acceleration","Dangerous Overspeeding","Overspeeding"};
+        ;
+
+        barchart_properties(barChart_trips);
+
     }
 
 
 
     private void barchart_properties(BarChart mbarChart){
 
-        mbarChart.setBackgroundColor(Color.rgb(64,224,208));
+        mbarChart.setBackgroundColor(Color.rgb(255,255,255));
         mbarChart.getDescription().setEnabled(false);
-        mbarChart.getLegend().setEnabled(false);
+        //mbarChart.getLegend().setEnabled(false);
         mbarChart.getAxisRight().setEnabled(false);
         mbarChart.setDrawGridBackground(false);
         mbarChart.getAxisLeft().setTextSize(13f);
